66476e144a0fda5ca3c65a7ee38323731b7b10bd
Paul John M. Santos <pauloletics@gmail.com>
2025-05-29 13:50:00 +0800

    Add dependencies

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ScnButton>
+    Hello
+  </ScnButton>
 </template>
+
+<script setup lang="ts">
+</script>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: 'Scn',
+    componentDir: './components/ui'
+  }
 })
