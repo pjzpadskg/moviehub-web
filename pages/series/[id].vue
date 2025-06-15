0b48ccdee506d521e32eddeafb7218c4aad5d970
PJ Santos <pauloletics@gmail.com>
2025-06-15 19:38:48 +0800

    Update [id].vue

--- pages/series/[id].vue
+++ pages/series/[id].vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 const route = useRoute()
-const embedUrl = computed(() => `https://embed.su/embed/tv/${route.params.id}`)
+const embedUrl = computed(() => {
+  const id = route.params.id
+  const season = route.query.season
+  const episode = route.query.episode 
+  return `https://embed.su/embed/tv/${id}/${season}/${episode}`
+})
 </script>
 
 <template>
-  <div class="h-screen w-screen bg-black text-white flex items-center justify-center">
-    <div class="w-screen h-screen">
+  <div class="bg-black text-white min-h-screen">
+    <header class="flex items-center justify-between px-6 py-4">
+    <NuxtLink
+      to="/"
+      class="text-3xl font-bold"
+    >
+      MovieHub
+    </NuxtLink>
+    </header>
+
+    <div class="px-8 w-screen h-screen">
       <iframe
         :src="embedUrl"
         class="w-full h-full"
